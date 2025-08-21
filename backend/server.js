72153635a2a14ac54e33ddd563d9bd3ad0c3d884
saitejareddyy <saitejareddy0502@gmail.com>
2025-08-21 10:31:09 +0530

    Update server.js, cors error

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,10 @@ const allowedOrigins = [
 ]
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if(!origin || allowedOrigins.includes(origin){
-      callback(null, true)
-    }
-    else{
+ origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
@@ -45,6 +44,7 @@ app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
   dbConnection();
 })
+
 
 
 
