83b4341c7da3b3962b4f0bc19c998f87354df883
saitejareddyy <saitejareddy0502@gmail.com>
2025-08-22 22:53:33 +0530

    Update App.jsx
    changed backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,7 @@ import AddSubject from './pages/AddSubject'
 import AllSubjects from './pages/AllSubjects'
 import {Toaster} from 'react-hot-toast'
 
-export const backendUrl = "http://localhost:5050"
+export const backendUrl = "https://paper-stack-backend.onrender.com"
 
 function App() {
   return (
