553d0994bf6162a4b130baa778850b3a5afa9d83
saitejareddyy <saitejareddy0502@gmail.com>
2025-08-22 23:05:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import { Toaster } from 'react-hot-toast'
 
 
 import StudentNotes from './pages/StudentNotes'
-export const backendUrl = "https://paper-stack-backend.onrender.com"
+export const backendUrl = "https://paper-stack-backend.onrender.com";
 
 
 function App() {
