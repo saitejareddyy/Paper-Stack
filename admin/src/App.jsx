b9a1079085a16623800c4a995a10511c347a3dd7
saitejareddyy <saitejareddy0502@gmail.com>
2025-08-23 10:58:13 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,7 @@ import AddSubject from './pages/AddSubject'
 import AllSubjects from './pages/AllSubjects'
 import {Toaster} from 'react-hot-toast'
 
-export const backendUrl = "https://paper-stack-backend.onrender.com"
+ export const backendUrl = "https://paper-stack-backend.onrender.com"
 
 function App() {
   return (
@@ -21,3 +21,6 @@ function App() {
 }
 
 export default App
+
+
+
